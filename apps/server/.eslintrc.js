c213255abbb65c22777be9c48d39c050cde75935
YusufAcala <167275609+YusufAcala@users.noreply.github.com>
2026-06-02 17:59:01 +0700

    menambahkan .eslintrc.js

---
+++ apps/server/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: [
+    ".eslintrc.js",
+    "dist",
+    "node_modules",
+  ],
+  rules: {},
+};
